2e615446d44b7ff974f6343a7ee970844d64e802
RAFIQUL595 <rafikulislam100000@gmail.com>
2024-10-28 12:31:29 +0600

    secend commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import NavBars from "./components/NavBars/NavBars";
 
 function App() {
   return (
     <>
+    <NavBars></NavBars>
       <NavBar></NavBar>
     </>
   );
